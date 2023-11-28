b34c9eeae2182d4d0f432e70b323ae0ea097a7e0
zahrabateninia <zahrabatenin@gmail.com>
2023-11-28 14:36:23 +0330

    Add data attributes to the gameboard cells

--- game.js
+++ game.js
@@ -1,19 +1,6 @@
-// Planning the Structure
-// Gameboard Object (Factory):
-
-// Represents the gameboard as an array.
-// Includes methods for handling moves, checking for a winner, and resetting the board.
-// Player Objects (Factory):
-
-// Represents individual players.
-// May include properties like name, symbol (X or O), etc.
-// Game Flow Controller Object (Factory or Module):
-
-// Manages the flow of the game.
-// Handles turns, checks for game end conditions (win or draw), and restarts the game
-
 const Gameboard = (()=>{
     let gameboard = ["","","","","","","","",""];
+    
     return{
 
     }
