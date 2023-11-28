95368819335f5245f0261215c14f8b038f638e58
zahrabateninia <zahrabatenin@gmail.com>
2023-11-28 17:16:19 +0330

    Add winningCombinations array

--- game.js
+++ game.js
@@ -13,6 +13,18 @@ const Gameboard = (()=>{
         return false;
     };
 
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6] 
+    ];
+    
+
     const resetGame= ()=>{
         gameboard = ["","","","","","","","",""];
 
@@ -25,6 +37,7 @@ const Gameboard = (()=>{
     return{
         getGameboard,
         placeMarker,
+        winningCombinations,
         resetGame,
     };
 })();
