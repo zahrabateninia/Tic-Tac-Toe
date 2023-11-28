d3ba43a17bdc25d9f8569bf8f33220bf8d509359
zahrabateninia <zahrabatenin@gmail.com>
2023-11-28 16:50:08 +0330

    Add placeMarker function in the Gameboard factory

--- game.js
+++ game.js
@@ -1,9 +1,18 @@
 const Gameboard = (()=>{
     let gameboard = ["","","","","","","","",""];
+
     const getGameboard= ()=>{
         return gameboard;
     };
-    
+
+    const placeMarker= (index, marker)=>{
+        if(gameboard[index]==""){
+            gameboard[index]= marker;
+            return true;
+        }
+        return false;
+    }
+
     return{
 
     }
